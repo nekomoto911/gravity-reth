61abcf1de8ff1477865fee4478791b90666fe684
Lightman <31928846+Lchangliang@users.noreply.github.com>
2025-01-20 16:31:13 +0800

    [improve] Add annotation for GravityStorage (#30)

--- crates/gravity-storage/src/lib.rs
+++ crates/gravity-storage/src/lib.rs
@@ -38,17 +38,22 @@ impl std::fmt::Display for GravityStorageError {
 pub trait GravityStorage: Send + Sync + 'static {
     type StateView: DatabaseRef<Error = ProviderError>;
 
+    // get state view for execute
     fn get_state_view(
         &self,
         block_number: u64,
     ) -> Result<(B256, Self::StateView), GravityStorageError>;
 
+    // Insert the mapping from block_number to block_id
     fn insert_block_id(&self, block_number: u64, block_id: B256);
 
+    // Insert the mapping from block_number to bundle_state
     fn insert_bundle_state(&self, block_number: u64, bundle_state: &BundleState);
 
-    fn update_canonical(&self, block_number: u64, block_hash: B256); // gc
+    // Update canonical to block_number and reclaim the intermediate result cache
+    fn update_canonical(&self, block_number: u64, block_hash: B256);
 
+    // calculate state root by block_number
     fn state_root_with_updates(
         &self,
         block_number: u64,
